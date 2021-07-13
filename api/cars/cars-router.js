544527d40945b5ba2ce941321340112c514e5b11
davedwight <dpdwight@gmail.com>
2021-07-13 17:07:30 -0600

    stubs out cars-router, successfully routing to /api/cars

--- api/cars/cars-router.js
+++ api/cars/cars-router.js
@@ -1,1 +1,11 @@
-// DO YOUR MAGIC
+const router = require('express').Router();
+
+// const Car = require('./cars-model');
+
+// import middleware here
+
+router.get('/', (req, res, next) => {
+    console.log('inside cars-router');
+})
+
+module.exports = router;
