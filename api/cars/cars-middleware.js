22fa32f71e4bc02b1911ea234430a49c5b708c6e
davedwight <dpdwight@gmail.com>
2021-07-13 22:39:20 -0600

    checkCarPayload working

--- api/cars/cars-middleware.js
+++ api/cars/cars-middleware.js
@@ -11,15 +11,38 @@ const checkCarId = async (req, res, next) => {
       next({
         status: 404,
         message: `car with id ${id} is not found`,
-      })
+      });
     }
   } catch (err) {
-    next(err)
+    next(err);
   }
 };
 
 const checkCarPayload = (req, res, next) => {
-  // DO YOUR MAGIC
+  const { vin, make, model, mileage } = req.body;
+
+  const nextFunc = (fieldName) => {
+    next({
+      status: 400,
+      message: `${fieldName} is missing`,
+    });
+  };
+
+  if (!vin) {
+    let fieldName = "vin";
+    nextFunc(fieldName);
+  } else if (!make) {
+    let fieldName = "make";
+    nextFunc(fieldName);
+  } else if (!model) {
+    let fieldName = "model";
+    nextFunc(fieldName);
+  } else if (!mileage) {
+    let fieldName = "mileage";
+    nextFunc(fieldName);
+  } else {
+    next();
+  }
 };
 
 const checkVinNumberValid = (req, res, next) => {
